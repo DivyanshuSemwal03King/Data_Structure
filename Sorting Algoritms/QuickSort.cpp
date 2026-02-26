89482948d2c9d9cc0d0f214b432552a08e1d2977
DivyanshuSemwal03King <divyanshusemwal03king@gmail.com>
2026-02-26 14:01:35 +0530

    Quick Sort is Completed

---
+++ Sorting Algoritms/QuickSort.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void swap(int& a, int& b){
+    int temp = a;
+    a = b;
+    b = temp;
+   
+}
+int partition(vector<int>& arr,int low,int high){
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while(i < j){
+        while(arr[i] <= pivot && i <= high - 1){
+            i++;
+        }
+        while(arr[j] > pivot && j >= low + 1){ 
+            j--;
+        }
+        if(i < j){
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[low], arr[j]);
+    return j;
+}
+void qs(vector<int>& arr,int low,int high){
+    if(low<high){
+        int pi = partition(arr,low,high);
+        qs(arr,low,pi-1);
+        qs(arr,pi+1,high);
+    }
+}
+
+int main(){
+    vector<int> arr = {38, 27, 43, 3, 9, 82, 10};
+    int n = arr.size();
+    qs(arr,0,n-1);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }   
+    return 0;
+}
